03632e71fac4a71a0feb7d851d7ea4e5c2483f5b
Khalid <khalid040804@gmail.com>
2023-03-16 14:20:57 +0100

    main java een test toegevoegd om output te checken
    Optie classe en OptieGegevens klasse aangemaakt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ObjectStorage.addKlanttype();
+
+        //testOptie
+        List<Optie> opties = new ArrayList<>();
+        Optie.voegOptieToe(opties);
+        opties.add(new Optie(new OptieGegevens("Airco", "Een airconditioning systeem", "Essentieel")));
+        System.out.println();
+        Optie.toonOpties(opties);
+        System.out.println();
+        Optie.verwijderOptie(opties);
+        System.out.println();
+        Optie.toonOpties(opties);
+        System.out.println();
+        Optie.bewerkOptie(opties);
+        System.out.println();
+        Optie.toonOpties(opties);
+
     }
 }
