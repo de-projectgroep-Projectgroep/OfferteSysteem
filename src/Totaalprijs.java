bc55ed933125e30835a88ccf1c9df504bdfd5298
rashadwhy <anwar.muradin@gmail.com>
2023-04-04 22:55:13 +0200

    eerste versie Totaalprijs... open voor review

--- src/Totaalprijs.java
+++ src/Totaalprijs.java
@@ -1,8 +1,38 @@
-public class Totaalprijs {
+///public class Totaalprijs {
     /*
      totaalprijs
      automatisch berekenen op basis klanttype en geselecteerde opties
      gemakkelijke interface
      korting: klanttype.java
     */
+}
+
+
+public class Totaalprijs {
+
+    private Klanttype klanttype;
+    private Set<Option> geselecteerdeOpties;
+
+    public Totaalprijs(Klanttype klanttype, Set<Option> geselecteerdeOpties) {
+        this.klanttype = klanttype;
+        this.geselecteerdeOpties = geselecteerdeOpties;
+    }
+
+    public double berekenTotaalprijs() {
+        double totaalprijs = 0.0;
+        double optieprijs = 0.0;
+
+        for (Option optie : geselecteerdeOpties) {
+            optieprijs += optie.getPrijs();
+        }
+
+        if (klanttype instanceof Bedrijfsklant) {
+            totaalprijs += optieprijs * 0.9; // 10% korting voor bedrijfsklanten
+        } else if (klanttype instanceof ParticuliereKlant) {
+            totaalprijs += optieprijs;
+        }
+
+        return totaalprijs;
+    }
+
 }
