c8f2c235c9152dca717df3f687033c962485a79c
BadZout <martijn.tas5@gmail.com>
2023-04-06 02:18:40 +0200

    menu.start() aanvragen via main methode

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
 public class Main {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
     }
 }
