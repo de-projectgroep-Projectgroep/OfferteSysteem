63fda2e81d443eee3a4d4bbbe0767ac8ed2c8414
The Eternal Emperor <22065156@student.hhs.nl>
2023-04-06 13:48:35 +0200

    Basis klanttype

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Klanttype basis = new Klanttype("Basis", 0);
         Menu menu = new Menu();
         menu.start();
     }
